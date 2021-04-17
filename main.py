8316a9eef14f774c6b5e96f69eccbc57b7e1f344
MrMarkofanYT <82262504+MrMarkofanYT@users.noreply.github.com>
2021-04-17 15:16:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-print("HIHIHIHI")
-print("HUHUHUHUHU")
+
+#импорт
+from pygame import *
+#окно
+okno = display.set_mode((300,200))
+fon = transform.scale(image.load("images.jpg"), (300, 200))
+#класс спрайта
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image,player_x,player_y,size_x,size_y):
+        sprite.Sprite.__init__(self)
+        self.image=transform.scale(image.load(player_image),(size_x,size_y))
+        self.rect=self.image.get_rect()
+        self.rect.x=player_x
+        self.rect.y=player_y
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+
+#цикл игры
+game = True
+while game:
+    for e in event.get():
+            if e.type==QUIT:
+                run=False
+    okno.blit(fon,(0, 0))
